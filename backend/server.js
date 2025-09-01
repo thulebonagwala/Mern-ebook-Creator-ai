dbb866b6693bf060f35925af31074d89729fd737
thulebonagwala <thulebonagwala@gmail.com>
2025-09-01 18:20:28 +0200

    Setup MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
 
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 // Middleware
 app.use(express.json());
