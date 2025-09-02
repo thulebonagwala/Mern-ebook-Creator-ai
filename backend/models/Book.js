df09e3015b9377769d5d54ce3f48ee239aba31d3
thulebonagwala <thulebonagwala@gmail.com>
2025-09-02 14:59:40 +0200

    Add Schema for books

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -14,3 +14,38 @@ const chapterSchema = new mongoose.Schema({
     default: "",
   },
 });
+
+const bookSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      default: "",
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+      default: "",
+    },
+    chapters: [chapterSchema],
+    status: {
+      type: String,
+      enum: ["draft", "published"],
+      default: "draft",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Book", bookSchema);
