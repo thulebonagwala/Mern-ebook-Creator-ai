94f4faf18d14487bdc375a83a94763379dcd3ac5
thulebonagwala <thulebonagwala@gmail.com>
2025-09-03 09:50:16 +0200

    Hash pasword for storage

--- backend/models/User.js
+++ backend/models/User.js
@@ -31,3 +31,18 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// Password hashing middleware
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Method to compare password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
