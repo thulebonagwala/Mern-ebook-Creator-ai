14341275767108b2513781dac2e30f054740b0ae
thulebonagwala <thulebonagwala@gmail.com>
2025-09-03 09:32:18 +0200

    Add user Schema

---
+++ backend/models/User.js
@@ -0,0 +1,33 @@
+// models/User.js
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      select: false,
+    },
+    avatar: {
+      type: String,
+      default: "",
+    },
+    isPro: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
