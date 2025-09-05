e2b13f457bcc4f11b41562b16ce8d4f61129435c
thulebonagwala <thulebonagwala@gmail.com>
2025-09-05 09:40:14 +0200

    Create apploads directory

---
+++ backend/middlewares/uploadMiddleware.js
@@ -0,0 +1,9 @@
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+// Create uploads directory if it doesn't exist
+const uploadDir = "uploads";
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
