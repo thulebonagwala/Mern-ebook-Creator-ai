63cbcf08eb4cd58e2d9f09e656c318670f403fe7
thulebonagwala <thulebonagwala@gmail.com>
2025-09-05 09:55:23 +0200

    Setup engine storage

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -7,3 +7,15 @@ const uploadDir = "uploads";
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
