fb71017cb2ddd144b3bbdbe0bf1f2ec4d4ac684f
thulebonagwala <thulebonagwala@gmail.com>
2025-09-05 10:10:28 +0200

    Add function to check file type

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -19,3 +19,16 @@ const storage = multer.diskStorage({
     );
   },
 });
+
+// Check file type
+function checkFileType(file, cb) {
+  const filetypes = /jpeg|jpg|png|gif/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Images Only!");
+  }
+}
