00c5850310b76a194874374f481231a846775855
thulebonagwala <thulebonagwala@gmail.com>
2025-09-07 11:39:42 +0200

    Add controller for user login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -40,3 +40,28 @@ exports.registerUser = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+// @desc    Login user
+// @route   POST /api/auth/login
+// @access  Public
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email }).select("+password");
+
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        message: "Login successful",
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
