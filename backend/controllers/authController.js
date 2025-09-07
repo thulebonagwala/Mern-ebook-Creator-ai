f8c21dddd308a3f3eca761f538d2168032d3c6ed
thulebonagwala <thulebonagwala@gmail.com>
2025-09-07 12:38:41 +0200

    Implement controller for updating profile

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -83,3 +83,27 @@ exports.getProfile = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+// @desc    Update user profile
+// @route   PUT /api/auth/me
+// @access  Private
+exports.updateUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+
+    if (user) {
+      user.name = req.body.name || user.name;
+
+      const updatedUser = await user.save();
+
+      res.json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+      });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
