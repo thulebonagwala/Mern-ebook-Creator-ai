cf16410a28ac547e1bc89eb7496c6a14b9431594
thulebonagwala <thulebonagwala@gmail.com>
2025-09-09 13:00:46 +0200

    Add controller to update book cover

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -82,3 +82,32 @@ const updateBook = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+// @desc    Update a book's cover image
+// @route   PUT /api/books/cover/:id
+// @access  Private
+const updateBookCover = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    if (book.userId.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "Not authorized to update this book" });
+    }
+
+    if (req.file) {
+      book.coverImage = `/${req.file.path}`;
+    } else {
+      return res.status(400).json({ message: "No image file provided" });
+    }
+
+    const updatedBook = await book.save();
+
+    res.status(200).json(updatedBook);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
