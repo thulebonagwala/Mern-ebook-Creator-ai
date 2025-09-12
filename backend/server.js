c5026da1a5921f067f4c8cb6506caf4bd2593e42
thulebonagwala <thulebonagwala@gmail.com>
2025-09-12 16:19:28 +0200

    Finalise server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const path = require("path");
 const connectDB = require("./config/db");
 
+const authRoutes = require('./routes/authRoutes');
+const bookRoutes = require('./routes/bookRoutes');
+const aiRoutes = require('./routes/aiRoutes');
+const exportRoutes = require('./routes/exportRoutes');
 
 const app = express();
 
@@ -19,6 +25,17 @@ connectDB();
 
 // Middleware
 app.use(express.json());
+
+
+// Static folder for uploads
+app.use("/backend/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Routes Here
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
+app.use("/api/ai", aiRoutes);
+app.use("/api/export", exportRoutes);
+
 
 // Start Server
 const PORT = process.env.PORT || 5000;
