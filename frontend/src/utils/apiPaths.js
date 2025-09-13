3eb0bbf7aedd59a528c1a1fb4312003c503caf7e
thulebonagwala <thulebonagwala@gmail.com>
2025-09-13 18:20:55 +0200

    Create API route for authenthication

---
+++ frontend/src/utils/apiPaths.js
@@ -0,0 +1,10 @@
+export const BASE_URL = "http://localhost:8000";
+
+export const API_PATHS = {
+  AUTH: {
+    REGISTER: "/api/auth/register",
+    LOGIN: "/api/auth/login",
+    GET_PROFILE: "/api/auth/profile",
+    UPDATE_PROFILE: "/api/auth/profile",
+  },
+}
