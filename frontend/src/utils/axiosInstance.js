ffb07cecb8b08d22b22d9bc55706459e0d052b60
thulebonagwala <thulebonagwala@gmail.com>
2025-09-14 18:00:48 +0200

    Add axios request interceptor

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -10,4 +10,18 @@ const axiosInstance = axios.create({
   },
 });
 
+// Request Interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
