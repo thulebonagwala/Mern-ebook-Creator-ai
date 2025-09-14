73fb55cee1cfe0f9a69a28fbe9835185b708fffb
thulebonagwala <thulebonagwala@gmail.com>
2025-09-14 18:31:04 +0200

    Add axios response interceptor

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -24,4 +24,22 @@ axiosInstance.interceptors.request.use(
   }
 );
 
+// Response Interceptor
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    // Handle common errors globally
+    if (error.response) {
+     if (error.response.status === 500) {
+        console.error("Server error. Please try again later.");
+      }
+    } else if (error.code === "ECONNABORTED") {
+      console.error("Request timeout. Please try again.");
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
